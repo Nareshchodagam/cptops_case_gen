5eb19a0fcf533b825eafde740720850bcaddd740
Donal Sheehan <dsheehan@dsheehan-ltm1.internal.salesforce.com>
2016-05-05 13:05:38 +0100

    lhub canary creation scripting

--- bin/dva_create_canary_cases.sh
+++ bin/dva_create_canary_cases.sh
@@ -4,8 +4,8 @@ BUNDLE=$1
 PATCHJSON=$2
 PREAMBLE=$3 #eg "FEB and GLIBC Patch Bundle : "
 SIGNOFFTEAM=$4 # one of LOG_TRANSPORT LOG_ANALYTICS DATA_BROKER ARGUS ALERTING
-
 OTHER="-T"
+PLAN=6u6-plan
 EXCLUDE='canaryhosts'
 FILE_SPL_SWI_CRZ='../hostlists/file_spl_swi_crz'
 FILE_SPL_WEB_CRZ='../hostlists/file_spl_web_crz'
@@ -20,28 +20,9 @@ function create_case {
 SUBJECT=$1
 DC=$2
 DCUP="$(echo $DC | awk '{print toupper($0)}')"
-
-echo "/usr/local/bin/python gus_cases.py -T change -f ../templates/$PATCHJSON -s \"$SUBJECT\" -k ../templates/6u6-plan.json -l ../output/summarylist.txt -D $DCUP -i ../output/plan_implementation.txt --infra \"Supporting Infrastructure\"" 
-/usr/local/bin/python gus_cases.py -T change -f ../templates/$PATCHJSON -s "$SUBJECT" -k ../templates/6u6-plan.json -l ../output/summarylist.txt -D $DCUP -i ../output/plan_implementation.txt --infra "Supporting Infrastructure" 
-}
-
-function build_case {
-
-DC=$1
-ROLE=$2
-PREAMBLE=$3
-CTYPE=$4
-STATUS=$5
-GROUPSIZE=$6
-GROUPING=$7
-TEMPLATEID=$8
-if [ -z "$TEMPLATEID" ]; then TEMPLATEID=$ROLE; fi
-
-./build_plan.py -c 0000001 -C -G '{"clusterTypes" : "'$CTYPE'" ,"datacenter": "'$DC'", "roles": "'$ROLE'", "grouping": "'$GROUPING'", "templateid" : "'$TEMPLATEID'",  "maxgroupsize": '$GROUPSIZE', "ho_opstat" : "'$STATUS'" }'  $OTHER --exclude $EXCLUDE --bundle $BUNDLE || exit 1
-
-SUBJECT="$PREAMBLE $DC $ROLE PROD"
-create_case "$SUBJECT" $DC
-
+PLAN=$4
+echo "/usr/local/bin/python gus_cases.py -T change -f ../templates/$PATCHJSON -s \"$SUBJECT\" -k ../templates/$PLAN -l ../output/summarylist.txt -D $DCUP -i ../output/plan_implementation.txt --infra \"Supporting Infrastructure\"" 
+/usr/local/bin/python gus_cases.py -T change -f ../templates/$PATCHJSON -s "$SUBJECT" -k ../templates/$PLAN -l ../output/summarylist.txt -D $DCUP -i ../output/plan_implementation.txt --infra "Supporting Infrastructure" 
 }
 
 function build_case_hostlist {
@@ -52,13 +33,12 @@ PREAMBLE=$3
 HOSTLIST=$4
 GROUPSIZE=$5
 GROUPING=$6
-TEMPLATEID=$7
 if [ -z "$TEMPLATEID" ]; then TEMPLATEID=$ROLE; fi
 
 ./build_plan.py -l $HOSTLIST -x -M $GROUPING --gsize $GROUPSIZE --bundle $BUNDLE -t $TEMPLATEID $OTHER || exit 1
 
 SUBJECT="$PREAMBLE $DC $ROLE PROD"
-create_case "$SUBJECT" $DC
+create_case "$SUBJECT" $DC $PLAN
 
 }
 
@@ -70,13 +50,12 @@ PREAMBLE=$3
 HOSTLIST=$4
 GROUPSIZE=$5
 GROUPING=$6
-
-
+PLAN=$7
 ./build_plan.py -l $HOSTLIST -M $GROUPING --gsize $GROUPSIZE --bundle $BUNDLE $OTHER || exit 1
 
 
 SUBJECT="$PREAMBLE $DC $ROLE PROD"
-create_case "$SUBJECT" $DC
+create_case "$SUBJECT" $DC $ROLE $PLAN
 }
 
 function build_case_extra {
@@ -102,12 +81,12 @@ echo "TITLE will be $MYSUBJECT"
 }
 
 
-
-build_case_hostlist_idb sfm CANARY "$3 SMARTS " ../hostlists/dva_smarts.canary 1 role
-build_case_hostlist_idb asg,crz CANARY "$3 LOGANALYTICS " ../hostlists/dva_loganalytics.canary 1 role
-build_case_hostlist_idb asg,sfm,sfz CANARY "$3 LOGTRANSPORT " ../hostlists/dva_log_transport.canary 1 role
-build_case_hostlist_idb asg CANARY "$3 DATABROKER " ../hostlists/dva_databroker.canary 1 role
-build_case_hostlist_idb sjl,sfz,chi,asg CANARY "$3 SR TOOLS " ../hostlists/dva_sr_sr_tools.canary 1 role
-build_case_hostlist_idb crd CANARY  "$3 CMS " ../hostlists/cms.canary 1 role
-build_case_hostlist sfm CANARY  "$3 DELPHI GACKPARSER " ../hostlists/dva_delphi.canary 1 role manage_apps-patch§
+build_case_hostlist_idb sfm CANARY "$3 SMARTS " ../hostlists/dva_smarts.canary 1 role $PLAN
+build_case_hostlist_idb asg,crz CANARY "$3 LOGANALYTICS " ../hostlists/dva_loganalytics.canary 1 role $PLAN
+build_case_hostlist_idb sfz CANARY "$3 LOGTRANSPORT LHUB" ~/lhubcanary 1 role lhub-plan.json
+build_case_hostlist_idb asg,sfm,sfz CANARY "$3 LOGTRANSPORT" ../hostlists/dva_logtransport.canary 1 role $PLAN
+build_case_hostlist_idb asg CANARY "$3 DATABROKER " ../hostlists/dva_databroker.canary 1 role $PLAN
+build_case_hostlist_idb sjl,sfz,chi,asg CANARY "$3 SR TOOLS " ../hostlists/dva_sr_sr_tools.canary 1 role $PLAN
+build_case_hostlist_idb crd CANARY  "$3 CMS " ../hostlists/cms.canary 1 role $PLAN
+build_case_hostlist sfm CANARY  "$3 DELPHI GACKPARSER " ../hostlists/dva_delphi.canary 1 role manage_apps-patch
 build_case_hostlist prd CANARY "$3 ARGUS " ../hostlists/dva_argus.canary 1 role manage_apps-patch
